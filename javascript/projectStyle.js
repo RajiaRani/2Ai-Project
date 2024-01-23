1a5d500a07e69d8c9de524ddee30b2149ead6c16
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-23 09:48:20 -0600

    updated

---
+++ javascript/projectStyle.js
@@ -0,0 +1,7 @@
+document.getElementById('clickableImage').addEventListener('click', function() {
+    document.getElementById('fullImageOverlay').style.display = 'flex';
+});
+
+document.getElementById('fullImageOverlay').addEventListener('click', function() {
+    document.getElementById('fullImageOverlay').style.display = 'none';
+});
