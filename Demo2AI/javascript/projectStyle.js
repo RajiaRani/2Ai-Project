c2edcd46bdf82a72362a693b330e6d675c545e2a
Rajia Rani <rajiarani.syal@gmail.com>
2024-01-28 11:41:40 -0600

    added

---
+++ Demo2AI/javascript/projectStyle.js
@@ -0,0 +1,8 @@
+
+document.getElementById('clickableImage').addEventListener('click', function() {
+    document.getElementById('fullImageOverlay').style.display = 'none';
+});
+
+document.getElementById('fullImageOverlay').addEventListener('click', function() {
+    document.getElementById('fullImageOverlay').style.display = 'none';
+});
